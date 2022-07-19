b4053dc5da9ef4a19bf5ec86cb765066bf083039
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-19 18:56:35 +0300

    Stage 1/5: Lay the groundwork

---
+++ Games/Minesweeper/part1/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Stage 1/5: Lay the groundwork
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".X.......");
+        System.out.println(".....X..X");
+        System.out.println("....X....");
+        System.out.println("......X..");
+        System.out.println("..X......");
+        System.out.println("....X....");
+        System.out.println("..X......");
+        System.out.println("..X......");
+        System.out.println("......X..");
+    }
+}

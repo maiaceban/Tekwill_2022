fb0d77c14e852d39619470fca17062ccf7f8e2c7
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-19 19:12:33 +0300

    Tic-Tac-Toe

---
+++ Games/TicTacToe/src/Part1.java
@@ -0,0 +1,7 @@
+public class Part1 {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}

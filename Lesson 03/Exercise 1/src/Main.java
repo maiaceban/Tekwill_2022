69ae7477cfe25f9deb9f89ac060800b3b0c03a2f
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-20 21:28:47 +0300

    Assignments

---
+++ Lesson 03/Exercise 1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    /**
+     * Java Fundamentals
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+        Change the code sample to make the output look the following way: example: 3 5 4
+        To do that, assign the correct values to the corresponding variables!
+         */
+        int ten = 10;
+        int oneHundred = 100;
+        int twelve = 12;
+        int eighty = 80;
+        int five = 5;
+        System.out.println(ten + "  " + oneHundred + "  " + twelve + "  " + eighty + " " + five);
+
+    }
+}

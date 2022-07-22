79cb6ee2ef3145472b2da44adbd636ba1b46f256
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 17:58:46 +0300

    Accounts

---
+++ Lesson 09/Exercise 01/src/Account.java
@@ -0,0 +1,12 @@
+/**
+ * Accounts
+ */
+/*
+ Define a class named Account. It must have three fields: a long field balance, string
+ field ownerName, and boolean flag locked.
+  */
+public class Account {
+    long balance;
+    String ownerName;
+    boolean flagLocked;
+}

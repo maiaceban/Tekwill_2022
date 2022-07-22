86758eeaffe6b6e49aa2fe3897360e1dc6105677
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 17:35:44 +0300

    Throw an unchecked exception (Throwing exceptions)

---
+++ Lesson 19/Execise 08/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Throw an unchecked exception (Throwing exceptions)
+ */
+/*
+Modify the given method. It should throw an unchecked exception.
+ */
+public class Main {
+    public static void method() {
+        // write your code here
+        throw new RuntimeException();
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+}

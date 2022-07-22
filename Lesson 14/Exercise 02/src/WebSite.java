534f1eea2a7b5d002f840edc280c9f2ce6aa22b2
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 18:16:33 +0300

    Users and WebSites

---
+++ Lesson 14/Exercise 02/src/WebSite.java
@@ -0,0 +1,12 @@
+public  class WebSite extends BaseEntity {
+    protected String url;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}

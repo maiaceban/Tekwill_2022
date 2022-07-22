cd5238655c8c12e33536601017caef3f0416d7ae
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 18:23:53 +0300

    Complex

---
+++ Lesson 09/Exercise 02/src/Complex.java
@@ -0,0 +1,10 @@
+public class Complex {
+    /*
+    You are creating a calculator that manipulates complex numbers. Write a class named Complex.
+    It must have two double fields named real and imaginary.
+     */
+    public static void main(String[] args) {
+        double real;
+        double imaginary;
+    }
+}

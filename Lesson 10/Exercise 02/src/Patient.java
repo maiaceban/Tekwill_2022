f3b285e7d5c15842b230add763a90a009fefaefa
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 18:23:09 +0300

    Patient

---
+++ Lesson 10/Exercise 02/src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+     /*
+Here's a class named Patient. It has one string field name. You want to create a
+method that would allow your patient to call for a doctor. Add the method say that
+prints the message containing the name of a patient (instead of "...") and their
+request for a doctor: Hello, my name is ..., I need a doctor.
+This method doesn't take any parameters and doesn't return anything.
+     */
+
+    String name;
+
+    public void say() {
+        System.out.printf("Hello, my name is %s, I need a doctor.", name);
+    }
+}

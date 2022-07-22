28f2996e7149c46e21447c5abe9402f78e355a6b
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 17:08:39 +0300

    Difference between two doubles

---
+++ Lesson 04/Exercise 6/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Difference between two doubles
+ */
+public class Main {
+    /*
+       Write a program that reads two double values and prints the difference between the
+       second and the first one.
+           Sample Input 1:
+           18.4
+           -5.0
+           Sample Output 1: -23.4
+        */
+    public static void main(String[] args) {
+        double a = 18.4;
+        double b = -5.0;
+        System.out.println(b - a);
+
+//        Scanner scanner = new Scanner(System.in);
+//        double d = scanner.nextDouble();
+//        double x = scanner.nextDouble();
+//        System.out.println(x - d);
+    }
+}

f6ed766833835ee7ece23f69b59e7e46f18e2de1
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-22 17:55:05 +0300

    Classes for Hospital

---
+++ Lesson 12/Exercise 01/src/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    /*
+In this problem, you need to develop a class hierarchy for the Hospital System.
+The root of the hierarchy is the Person class. It represents all employees and
+patients of the Hospital. Doctors are kind of employees.
+     */
+}

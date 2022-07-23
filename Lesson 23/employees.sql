5ceab36e30e41c52988d6cc5ac2e0dc0dfba4944
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-23 19:14:40 +0300

    uptate date

--- Lesson 23/employees.sql
+++ Lesson 23/employees.sql
@@ -34,7 +34,7 @@ select
 from
 	employees;
 
-
+-- Class Exercises:
 -- Ex 23.7 Where employees
 -- 1. select all emloyees that are younger than 45
 select * from employees where age < 45;
@@ -43,53 +43,39 @@ select * from employees where first_name = 'Lauren' or  last_name = 'Beaulieu';
 -- 3. select all employees that are older than 60 or younger than 20
 select * from employees where age > 60 or age < 20;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Ex 23.8 Order by DESC and ASC
+-- Ex 23.8 Sort employees
+-- 1. order employees by age in a descending order 
 select * from employees order by age desc;
-select * from employees order by first_name desc ;
+-- 2. order employees by first name in a descending order 
+select * from employees order by first_name desc;
+-- 3. order employees by age in a ascendant order and by last name in a descending order 
 select * from employees order by age asc, last_name desc;
 
--- Ex 23.9 Update employees
+-- Ex 23.9 Update employees 
+-- 1. Update the empployees with id=26, set his first name to 'Alexandru' and last name to 'cel Bun'
 select * from employees where id = 26;
-update employees  set first_name = 'Alexandru', last_name = 'cel Bun' where id = 26;
-select * from employees where first_name = 'Marv' ;
+update employees set first_name = 'Alexandru', last_name = 'cel Bun' where id = 26;
+-- 2. Update the employees with first_name = Marv by setting his age to 85
+select * from employees where first_name = 'Marv';
 update employees set age = 85 where first_name = 'Marv';
-select * from employees where age > 70;
+-- 3. Update all emloyees that are older than 70 to have the first name 'Abgar'
+select * from employees where  age > 70;
 update employees set first_name = 'Abgar' where age > 70;
 
--- Ex 23.10 delete employees
+-- ex 23.10 Delete employees
+-- 1. Delete all employees that are younger than 20
+-- select * from employees;
 delete from employees where age < 20;
-delete from employees where first_name ='Alexandru';
+-- 2. delete all employees with first name 'Abgar'
+delete from employees where first_name = 'Abgar';
 
--- Exx 23.11 Add new employees
+-- Ex 23.11 Add new employees
 select * from employees where email in ('maiacebanjava@gmail.com', 'joracardan@yahoo.com', 'asilegratii@mail.ru');
 insert into employees (first_name, last_name, email, age) 
-values ('Ceban', 'Maia', 'maiacebanjava@gmail.com', 26),
-('Jora', 'Cardan','joracardan@yahoo.com', 32),
-('Vasile', 'Gratii', 'asilegratii@mail.ru', 27);
-
+values 
+('Ceban', 'Maia', 'maiacebanjava@gmail.com', 26),
+('Joachim', 'Worsnip', 'jwornip0@sina.com.cn', 29),
+('Groian', 'Tatiana', 'groiantatiana@gmail.com', 38);
 
 -- Ex 23.12 Department table
 select * from departments;
@@ -110,8 +96,6 @@ alter table departments alter column department_id set not null;
 
 alter table departments alter column department_name set not null;
 alter table departments alter column department_name set default 'Unknown';
-
--- Ex 23.15  Primary key
 
 
 

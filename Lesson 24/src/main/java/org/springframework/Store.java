0c5f320a433b485045d492327073ba4b55ff104f
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-24 19:30:25 +0300

    Spring Context

---
+++ Lesson 24/src/main/java/org/springframework/Store.java
@@ -0,0 +1,17 @@
+package org.springframework;
+
+public class Store {
+//    private final Item item;
+//
+//    public Store() {
+//        item = new ItemImpl();
+//    }
+
+//    public Store(Item item) {
+//        this.item = item;
+//    }
+
+//    public void setItem(Item item) {
+//        this.item = item;
+//    }
+}

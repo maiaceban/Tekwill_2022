6904d5f6364582ed242676187964db02dc83f0d3
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-24 19:14:23 +0300

    First Maven Project

---
+++ Lesson 20/src/main/java/md/tekwill/Main.java
@@ -0,0 +1,10 @@
+package md.tekwill;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Alexandru cel bun";
+        System.out.println(StringUtils.capitalize(str));
+    }
+}

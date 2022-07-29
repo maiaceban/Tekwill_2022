2a984d6efd2279449c80621f41b61f33b2583e4e
Ceban Maia <maya.ceb.96@gmail.com>
2022-07-29 19:03:54 +0300

    lesson 25

---
+++ Lesson 25/src/main/java/md/tekwill/Test.java
@@ -0,0 +1,22 @@
+package md.tekwill;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) {
+        String url= "jdbc:postgresql://localhost:5432/tekwill_demo_db";
+        String username = "tekwill";
+        String password = "tekwill";
+
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            if (connection.isValid(5)){
+                System.out.println("Connection is valid");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}

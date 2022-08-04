7be1d08007489f82db54ca7c0a8a94706ef93703
Ceban Maia <maya.ceb.96@gmail.com>
2022-08-04 20:17:18 +0300

    ToDo App

--- Projects/ToDo App/todo-backend/src/main/java/md/tekwill/todo/repository/TaskRepository.java
+++ Projects/ToDo App/todo-backend/src/main/java/md/tekwill/todo/repository/TaskRepository.java
@@ -1,9 +1,10 @@
 package md.tekwill.todo.repository;
 
 import md.tekwill.todo.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> { //CrudRepository<Task, Long>
+public interface TaskRepository extends CrudRepository<Task, Long> { //JpaRepository<Task, Long>
 }
